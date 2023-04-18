5ae166ab80d92d3dae9f4ec61bc856e82d5fc2fd
CoderGaming-1 <96689912+CoderGaming-1@users.noreply.github.com>
2023-04-18 17:03:46 +0700

    Add files via upload

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static ShopLearning.Models.ApplicationUser;
+
+namespace ShopLearning.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Order
+        public ActionResult Index(int? page)
+        {
+            var item = db.Orders.OrderByDescending(x => x.CreatedDate).ToList();
+            if(page == null)
+            {
+                page = 1;
+            }
+            var pageNumber = page ?? 1;
+            var pageSize = 5;
+            ViewBag.Page = pageNumber;
+            ViewBag.PageSize = pageSize;
+            return View(item.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult View(int id)
+        {
+            var item = db.Orders.Find(id);
+            return View(item);
+        }
+        public ActionResult Partial_SanPham(int id)
+        {
+            var item = db.OrderDetails.Where(x => x.Id == id).ToList();
+            return PartialView(item);
+        }
+        [HttpPost]
+        public ActionResult UpdateTT(int id, int trangthai)
+        {
+            var item = db.Orders.Find(id);
+            if (item != null)
+            {
+                item.TypePayment = trangthai;
+                db.Entry(item).Property(x => x.TypePayment).IsModified = true;
+                db.SaveChanges();
+                return Json(new { message = "Success", Success = true });
+            }
+            return Json(new { message = "Failed", Success = false });
+        }
+    }
+}
---
+++ Controllers/RoleController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static ShopLearning.Models.ApplicationUser;
+
+namespace ShopLearning.Areas.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Role
+        public ActionResult Index()
+        {
+            var item = db.Roles.ToList();
+            return View(item);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IdentityRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+                roleManager.Create(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = db.Roles.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(IdentityRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+                roleManager.Update(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
